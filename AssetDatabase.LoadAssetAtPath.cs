ccdcc2a879bf9987a76bdcb85be7af5c766ef18c
tui1314 <tui1314@163.com>
2016-07-02 20:26:21 +0800

    Update AssetDatabase.LoadAssetAtPath.cs

--- AssetDatabase.LoadAssetAtPath.cs
+++ AssetDatabase.LoadAssetAtPath.cs
@@ -5,6 +5,12 @@
 //assetPath   :Path of the asset to load. 
 //type        :Data type of the asset. 
 //
+//Description
+//Returns the first asset object of type type at given path assetPath.
+//
+//Some asset files may contain multiple objects. (such as a Maya file which may contain multiple Meshes and GameObjects).
+//All paths are relative to the project folder, for example: "Assets/MyTextures/hello.png".
+
 //Example 1 : get avatar from a fbx file
 string avatarPath = importer.assetPath.Split('@')[0] + ".fbx";
 Avatar avatarFile = AssetDatabase.LoadAssetAtPath<Avatar>(avatarPath);
